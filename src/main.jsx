154d6cf4e4f4dec20e4dfa4771e06af73c6704d2
Abhinandan Yadav <abhinandan@onramp.money>
2025-06-18 01:30:02 +0530

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("SW registered:", reg))
+      .catch((err) => console.error("SW registration failed:", err));
+  });
+}
 
 
 const originalWarn = console.warn;
@@ -15,13 +24,8 @@ console.warn = (...args) => {
   originalWarn(...args); // let others pass through
 };
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <App />
+    <App />
   </StrictMode>
 );
-
-
-
-
